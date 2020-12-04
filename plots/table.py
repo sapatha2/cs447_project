c342bd6684019bdd421ef73fda962d2042decd5e
Shivesh A Pathak <shiveshpathak1@MacBook-Pro.local>
2020-12-03 18:29:24 -0600

    .

--- plots/table.py
+++ plots/table.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('bleu.csv')
 df['Params'] = df['parms']
 df = df[['Language','Model','Params','BLEU1','BLEU2','BLEU3','BLEU4']]
+for b in ['BLEU1','BLEU2','BLEU3','BLEU4']:
+    df[b] = np.around(df[b].values, 1)
 groups = df.groupby(by='Language')
 
 s = ''
-for g, d in groups:
-    s += g + ' \\\\ \n'
-    s += d.drop(columns=['Language']).to_latex(index = False) + ' \\\\ \n'
+for g in ['Italian','German','French','Spanish','Portugese']:
+    d = df[df['Language']==g]
+    s += '\\textbf{' + g + '} \\\\ \n'
+    s += d.drop(columns=['Language']).to_latex(index = False) + ' \\\\ \\\\ \n'
 
 f = open('table.tex','w')
 f.write(s)
