1de41dada55e0ffccd11281ebc5bf6170bd1a35d
Shivesh A Pathak <shiveshpathak1@MacBook-Pro.local>
2020-12-04 10:34:21 -0600

    barplot updates

--- plots/barplot.py
+++ plots/barplot.py
@@ -5,27 +5,21 @@ import matplotlib.pyplot as plt
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 df = pd.read_csv('bleu.csv')
 
-fig, ax = plt.subplots(nrows = 4, ncols = 2, sharex = True, figsize = (6, 9))
-for i, b in enumerate(['BLEU1','BLEU2','BLEU3','BLEU4']):
-    plotdf = df.pivot(index='Language', columns='Model', values = b)[['Deep Attention','Attention','Base']]
-    plotdf = plotdf.loc[['Italian','German','French','Spanish','Portugese']]
-    plotdf['Deep Attention'] = plotdf['Deep Attention'].values - plotdf['Attention'].values
-    plotdf['Attention'] = plotdf['Attention'].values - plotdf['Base'].values
-
-    plotdf[['Attention']].plot(kind = 'bar', ax = ax[i,0], rot = 40, width = 0.75, edgecolor='k', color=colors[0])
-    ax[i,0].set_xlabel('')
-    ax[i,0].set_ylabel(b)
-    ax[i,0].get_legend().remove()
-    ax[i,0].set_ylim((5, 15))
-    ax[i,0].set_yticks([5, 10, 15])
-
-    plotdf[['Deep Attention']].plot(kind = 'bar', ax = ax[i,1], rot = 40, width = 0.75, edgecolor='k', color=colors[2])
-    ax[i,1].set_xlabel('')
-    ax[i,1].get_legend().remove()
-    ax[i,1].set_ylim((0, 2.2))
-    ax[i,1].set_yticks([0, 1, 2])
-
-ax[0,0].set_title('Attention improvements')
-ax[0,1].set_title('Deep improvements')
+fig, ax = plt.subplots(1, 1, figsize = (3,3))
+d = df.pivot(index = 'Language', columns='Model', values = 'BLEU4')
+d = d.iloc[[2, 3, 4, 0, 1]]
+d = d[['Base','Attention','Deep Attention']]
+d['Deep Attention'] = d['Deep Attention'] - d['Attention']
+d['Attention'] = d['Attention'] - d['Base']
+d.columns = ['Base - Base', 'Base - Att', 'Deep - Att']
+d.plot.bar(rot = 40, stacked=True, ax = ax, 
+        color = {'Base - Base': colors[0], 'Base - Att': colors[1], 'Deep - Att': colors[3]}, 
+        edgecolor='k', width=0.75)
+plt.xlabel('')
+plt.ylabel('BLEU4')
+legend = plt.legend()
+frame = legend.get_frame()
+frame.set_facecolor('white')
+frame.set_edgecolor('black')
+frame.set_alpha(1.0)
 plt.savefig('barplot.pdf',bbox_inches='tight')
-plt.close()
